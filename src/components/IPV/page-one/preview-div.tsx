bf13b7b09c92d6d9b6d740d214630716461ba94d
harsh@technokart.in <harsh@technokart.in>
2024-02-29 16:53:44 +0530

    next/Image >> html/img

--- src/components/IPV/page-one/preview-div.tsx
+++ src/components/IPV/page-one/preview-div.tsx
@@ -1,30 +1,21 @@
 import React from "react";
-import Image from "next/image";
 
 function PreviewDiv(props: { isLoading: Boolean; preview: string }) {
   return (
     <div className={props.isLoading ? "preview-div-loading" : "preview-div"}>
       {props.isLoading ? (
         <div className="preview-image-loading">
-          <Image
+          <img
             src={props.preview}
             alt="Uploaded Document"
             className="preview-image"
-            width={300} // Set the width of the image
-            height={300} // Set the height of the image
-            // layout="responsive" // Set layout to responsive
-            style={{ objectFit: "contain" }}
           />
         </div>
       ) : (
-        <Image
+        <img
           src={props.preview}
           alt="Uploaded Document"
           className="preview-image"
-          layout="responsive" // Set layout to responsive
-          width={300} // Set the width of the image
-          height={300} // Set the height of the image
-          style={{ objectFit: "contain" }}
         />
       )}
     </div>
